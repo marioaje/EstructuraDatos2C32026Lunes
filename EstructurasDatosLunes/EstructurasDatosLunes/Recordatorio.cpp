9b50381f3263fc20923f82592284a062283d01e2
marioaje <marioaje@gmail.com>
2026-09-14 19:30:33 -0600

    Creacion de Lista

--- EstructurasDatosLunes/EstructurasDatosLunes/Recordatorio.cpp
+++ EstructurasDatosLunes/EstructurasDatosLunes/Recordatorio.cpp
@@ -1,6 +1,8 @@
 
 #include <iostream>
 #include "profesor.h"
+#include <cstring>
+#include "listas.h"
 
 int main() {
 	std::cout << "Recordatorio de Arreglo\n";
@@ -40,5 +42,23 @@ int main() {
 	profesorEstructura.edad = 30;
 	profesorEstructura.id = 1;
 	profesorEstructura.salario = 1000.50;
-	profesorEstructura.nombre = "Profe Mario";
+//	profesorEstructura.nombre = "Profe Mario";//?? el tipo de dato nos da el error
+	strcpy_s(profesorEstructura.nombre, "Profe Mario");//?? el tipo de dato nos da el error
+	//String (Clase) nombre.Upper() a string tipo de dato
+	// //varchar o nvarchar(100) o TEXT
+	//7*100
+	///"Profe Mario"
+	//nombre[50] =
+	//"P","r","o","f","e"," ","M","a","r","i","o","","","","","","","","","","","","","","","","","","","","",""
+
+	//Necesito corregir el problema del arreglo	int numeros[100];
+	// //int numeros[]
+	//0,1,2,3,4,5,6,7,8,9,0,11,21,121,23,4,5,6,7,8,9,//algo dinamico
+	//Listas???
+	//Memoria dinamica
+	Nodo* nodoListas = new Nodo();
+	nodoListas->dato = 10;
+	nodoListas->siguiente = nullptr;
+
+	return 0;
 }
